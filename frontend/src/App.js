58e8267f9a593857e4d412b3b14ec30a064e034a
Alexander Beers <alexbeerz@yahoo.com>
2020-06-09 21:27:20 -0700

    Initialize project.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App display-3">
+       
+      Hello There!
+     
+    </div>
+  );
+}
+
+export default App;
